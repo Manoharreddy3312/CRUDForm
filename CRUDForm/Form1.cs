16b3a60c887778b73bf20c2396c9814160809687
mosar <mosar@MANOHAR>
2024-12-16 01:55:14 +0530

    Add project files.

---
+++ CRUDForm/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=MANOHAR\\SQLEXPRESS;Initial Catalog=CRUDForm;Integrated Security=True;Trust Server Certificate=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ut values(@id,@name,@age)",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@age",double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Inserted");
+
+
+    }
+    }
+}
